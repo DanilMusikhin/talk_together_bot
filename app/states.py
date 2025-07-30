7632ed16acaf4f55c03007be753d7d6e58eb93fc
Danil <musikhin.danil@gmail.com>
2025-07-30 17:49:07 +0500

    Изменения для правильной работы states

--- app/states.py
+++ app/states.py
@@ -1,10 +1,10 @@
 """ | Файл с состояниями | """
 
 # Модули для управления состояниями
-from aiogram.fsm.state import State, StateGroup
+from aiogram.fsm.state import State, StatesGroup
 
 # Состояния для работы с базой данных database_handlers.py
-class DatabaseStates(StateGroup):
+class DatabaseStates(StatesGroup):
     CREATE = State()  # Состояние создания
     UPDATE = State()  # Состояние обновления
     DELETE = State()  # Состояние удаления
